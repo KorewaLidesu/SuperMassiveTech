3f81c54d951eb6ca368d3c76df0204e706a1eed5
Li <nhatlinh.l195@gmail.com>
2021-06-14 13:24:21 +0700

    Update BlockBlackHoleHopper.java

--- src/main/java/tterrag/supermassivetech/common/block/container/BlockBlackHoleHopper.java
+++ src/main/java/tterrag/supermassivetech/common/block/container/BlockBlackHoleHopper.java
@@ -1,90 +1,103 @@
-package tterrag.supermassivetech.common.block.container;
+package korewali.supermassivetech.client;
 
-import net.minecraft.block.material.Material;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.world.World;
-import tterrag.supermassivetech.SuperMassiveTech;
-import tterrag.supermassivetech.api.common.block.ISaveToItem;
-import tterrag.supermassivetech.common.tile.TileBlackHoleHopper;
-import tterrag.supermassivetech.common.util.Utils;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.client.model.obj.WavefrontObject;
+import korewali.supermassivetech.ModProps;
+import korewali.supermassivetech.client.model.ModelBlackHoleStorage;
+import korewali.supermassivetech.client.render.BlackHoleSpecialRenderer;
+import korewali.supermassivetech.client.render.ChargerSpecialRenderer;
+import korewali.supermassivetech.client.render.RenderStarHarvester;
+import korewali.supermassivetech.client.render.WaypointSpecialRenderer;
+import korewali.supermassivetech.client.render.entity.RenderDyingBlock;
+import korewali.supermassivetech.client.render.entity.RenderFormingStar;
+import korewali.supermassivetech.common.CommonProxy;
+import korewali.supermassivetech.common.entity.EntityDyingBlock;
+import korewali.supermassivetech.common.entity.EntityFormingStar;
+import korewali.supermassivetech.common.tile.TileBlackHole;
+import korewali.supermassivetech.common.tile.TileBlackHoleHopper;
+import korewali.supermassivetech.common.tile.TileBlackHoleStorage;
+import korewali.supermassivetech.common.tile.TileWaypoint;
+import korewali.supermassivetech.common.tile.energy.TileCharger;
+import korewali.supermassivetech.common.tile.energy.TileStarHarvester;
 
-public class BlockBlackHoleHopper extends BlockContainerSMT implements ISaveToItem
+import com.enderio.core.client.render.DirectionalModelRenderer;
+import com.enderio.core.client.render.SimpleModelRenderer;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
+import static korewali.supermassivetech.SuperMassiveTech.*;
+
+public class ClientProxy extends CommonProxy
 {
-    public BlockBlackHoleHopper()
+    public static DirectionalModelRenderer<TileBlackHoleStorage> storage;
+
+    public static DirectionalModelRenderer<TileBlackHoleHopper> hopper;
+
+    public static RenderStarHarvester starHarvester;
+
+    public static WaypointSpecialRenderer beacon;
+    public static SimpleModelRenderer waypoint;
+
+    public static SimpleModelRenderer charger;
+    public static ChargerSpecialRenderer chargerSpecial;
+
+    public static BlackHoleSpecialRenderer blackHole;
+
+    @Override
+    public void preInit()
     {
-        super("blackHoleHopper", Material.iron, soundTypeMetal, 30.0f, TileBlackHoleHopper.class, SuperMassiveTech.renderIDHopper);
+        renderIDStorage = RenderingRegistry.getNextAvailableRenderId();
+        renderIDHopper = RenderingRegistry.getNextAvailableRenderId();
+        renderIDStarHarvester = RenderingRegistry.getNextAvailableRenderId();
+        renderIDWaypoint = RenderingRegistry.getNextAvailableRenderId();
+        renderIDBlackHole = RenderingRegistry.getNextAvailableRenderId();
+        renderIDCharger = RenderingRegistry.getNextAvailableRenderId();
     }
 
     @Override
-    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ)
+    public void registerRenderers()
     {
-        TileBlackHoleHopper te = (TileBlackHoleHopper) world.getTileEntity(x, y, z);
+        storage = new DirectionalModelRenderer<TileBlackHoleStorage>(new ModelBlackHoleStorage(),
+                new ResourceLocation(ModProps.MOD_TEXTUREPATH, "textures/models/storage.png"));
+        hopper = new DirectionalModelRenderer<TileBlackHoleHopper>(new ResourceLocation(ModProps.MOD_TEXTUREPATH, "models/newHopper.obj"), new ResourceLocation(
+                ModProps.MOD_TEXTUREPATH, "textures/models/hopper.png"));
+        starHarvester = new RenderStarHarvester(new ResourceLocation(ModProps.MOD_TEXTUREPATH, "models/starHarvesterMain.obj"), new ResourceLocation(ModProps.MOD_TEXTUREPATH,
+                "models/starHarvesterSphere.obj"), new ResourceLocation(ModProps.MOD_TEXTUREPATH, "models/ring.obj"));
+        waypoint = new SimpleModelRenderer(new WavefrontObject(new ResourceLocation(ModProps.MOD_TEXTUREPATH, "models/waypoint.obj")), renderIDWaypoint);
+        beacon = new WaypointSpecialRenderer();
+        charger = new SimpleModelRenderer(new WavefrontObject(new ResourceLocation(ModProps.MOD_TEXTUREPATH, "models/charger.obj")), renderIDCharger);
+        chargerSpecial = new ChargerSpecialRenderer();
+        blackHole = new BlackHoleSpecialRenderer();
 
-        if (te == null || world.isRemote)
-            return true;
+        // BHS
+        ClientRegistry.bindTileEntitySpecialRenderer(TileBlackHoleStorage.class, storage);
+        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(blockRegistry.blackHoleStorage), storage);
 
-        ItemStack current = player.getCurrentEquippedItem();
+        // BHH
+        ClientRegistry.bindTileEntitySpecialRenderer(TileBlackHoleHopper.class, hopper);
+        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(blockRegistry.blackHoleHopper), hopper);
 
-        if (current == null)
-        {
-            if (player.isSneaking())
-                te.clearConfig(player);
-            else
-                player.addChatMessage(new ChatComponentText(Utils.lang.localize("tooltip.currentConfig") + ": " + te.getConfig()));
-        }
-        else
-        {
-            te.setConfig(current, player);
-        }
+        // Star Harvester
+        ClientRegistry.bindTileEntitySpecialRenderer(TileStarHarvester.class, starHarvester);
+        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(blockRegistry.starHarvester), starHarvester);
 
-        return true;
-    }
+        // Charger
+        ClientRegistry.bindTileEntitySpecialRenderer(TileCharger.class, chargerSpecial);
+        RenderingRegistry.registerBlockHandler(charger);
 
-    @Override
-    public ItemStack getNBTItem(World world, int x, int y, int z)
-    {
-        ItemStack stack = new ItemStack(this);
-        TileBlackHoleHopper te = (TileBlackHoleHopper) world.getTileEntity(x, y, z);
+        // Waypoint
+        ClientRegistry.bindTileEntitySpecialRenderer(TileWaypoint.class, beacon);
+        RenderingRegistry.registerBlockHandler(waypoint);
 
-        if (te == null)
-            return stack;
+        // Black Hole
+        ClientRegistry.bindTileEntitySpecialRenderer(TileBlackHole.class, blackHole);
+        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(blockRegistry.starHarvester), starHarvester);
 
-        stack.stackTagCompound = new NBTTagCompound();
-
-        if (te.getStackInSlot(0) != null)
-        {
-            NBTTagCompound tag0 = new NBTTagCompound();
-            te.getStackInSlot(0).writeToNBT(tag0);
-            stack.stackTagCompound.setTag("inventory0", tag0);
-        }
-        if (te.getStackInSlot(1) != null)
-        {
-            NBTTagCompound tag1 = new NBTTagCompound();
-            te.getStackInSlot(1).writeToNBT(tag1);
-            stack.stackTagCompound.setTag("inventory1", tag1);
-        }
-
-        return stack;
-    }
-
-    @Override
-    public void processBlockPlace(NBTTagCompound tag, TileEntity te)
-    {
-        if (te instanceof TileBlackHoleHopper)
-        {
-            TileBlackHoleHopper hopper = (TileBlackHoleHopper) te;
-            hopper.setInventorySlotContents(0, ItemStack.loadItemStackFromNBT(tag.getCompoundTag("inventory0")));
-            hopper.setInventorySlotContents(1, ItemStack.loadItemStackFromNBT(tag.getCompoundTag("inventory1")));
-        }
-    }
-
-    @Override
-    public boolean canPlaceTorchOnTop(World world, int x, int y, int z)
-    {
-        return false;
+        // Entities
+        RenderingRegistry.registerEntityRenderingHandler(EntityFormingStar.class, new RenderFormingStar());
+        RenderingRegistry.registerEntityRenderingHandler(EntityDyingBlock.class, new RenderDyingBlock());
     }
 }
